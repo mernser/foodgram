6045273596295a3993ab3f0943f795814c592454
Mudcrab Ernser <mernser@mi-t2.msk.21-school.ru>
2025-07-30 20:11:14 +0300

    30.07.25_20:11

--- backend/foodgram/api/models.py
+++ backend/foodgram/api/models.py
@@ -1,61 +1,18 @@
+import hashlib
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from foodgram.constants import (MAX_TAG_LENGTH,
                                 MAX_INGRIDIENT_NAME_LENGTH,
-                                MAX_UNIT_NAME_LENGTH,)
+                                MAX_UNIT_NAME_LENGTH,
+                                MAX_RECIPE_NAME_LENGTH,
+                                MIN_RECIPE_LENGTH_NAME,
+                                MAX_LINK_LENGTH,
+                                MAX_HASH_LENGTH)
 
-
-class Tag(models.Model):
-    name = models.CharField(
-        'название тега',
-        max_length=MAX_TAG_LENGTH,
-        unique=True
-    )
-    slug = models.SlugField(
-        'slug названия тега',
-        max_length=MAX_TAG_LENGTH,
-        unique=True
-    )
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
-
-    def __str__(self):
-        return f'{self.name}'
-
-
-# class Recipies(models.Model):
-#     image = models.ImageField('Картинка', upload_to='recipes/')
-#     name = models.CharField('Название', max_length=RECIPE_MAX_LENGTH_NAME)
-#     text = models.TextField('Описание')
-#     cooking_time = models.PositiveSmallIntegerField(
-#         'Время приготовления в минутах',
-#         validators=[
-#             MinValueValidator(
-#                 COOKING_MIN_TIME,
-#                 f'Значение не должно быть меньше {COOKING_MIN_TIME}',
-#             ),
-#             MaxValueValidator(
-#                 MAX_POSITIVE_VALUE,
-#                 f'Значение не должно быть больше {MAX_POSITIVE_VALUE}',
-#             ),
-#         ],
-#     )
-#     tags = models.ManyToManyField(Tag, verbose_name='Теги')
-#     ingredients = models.ManyToManyField(
-#         Ingredient, verbose_name='Ингредиенты', through='RecipeIngredient'
-#     )
-
-#     class Meta:
-#         ordering = ('-created_at',)
-#         default_related_name = 'recipes'
-#         verbose_name = 'Рецепт'
-#         verbose_name_plural = 'Рецепты'
-
-#     def __str__(self):
-#         return self.name
+User = get_user_model()
 
 
 class Ingredient(models.Model):
@@ -78,3 +35,130 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'название тега',
+        max_length=MAX_TAG_LENGTH,
+        unique=True
+    )
+    slug = models.SlugField(
+        'slug тега',
+        max_length=MAX_TAG_LENGTH,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Recipies(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='recipes/',
+        blank=False,
+        help_text='Изображение блюда'
+    )
+    name = models.CharField(
+        'Название',
+        max_length=MAX_RECIPE_NAME_LENGTH,
+        blank=False,
+        help_text='Наименование рецепта'
+    )
+    text = models.TextField(
+        'Описание',
+        blank=False,
+        help_text='Описание рецепта',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления (в минутах)',
+        validators=(
+            MinValueValidator(
+                MIN_RECIPE_LENGTH_NAME,
+                f'Значение не должно быть меньше {MIN_RECIPE_LENGTH_NAME}',
+            ),),
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиенты',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    short_link = models.CharField(
+        'Уникальный хеш для генерации ссылки',
+        max_length=MAX_LINK_LENGTH,
+        blank=True,
+        editable=False,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.short_link:
+            self.short_link = (
+                hashlib.sha256(
+                    f'{self.pub_date.isoformat()}{self.author.id}'
+                    .encode('utf-8')).hexdigest())[:MAX_HASH_LENGTH]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('created_at',)
+        default_related_name = 'recipes'
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipies,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент',
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество',
+        validators=[
+            MinValueValidator(
+                1,
+                'Количество не может быть меньше 1'
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
+        defualt_related_name = 'recipe_ingredients'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('recipe', 'ingredient'),
+                name='unique_ingredient_in_recipe'
+            )
+        )
+
+    def __str__(self):
+        return f'{self.ingredient.name} - {self.amount}'
